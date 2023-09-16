5dcbbf2a6844ace8b16ef531759134c6e46d1c10
Di <144306984+Jrklmn@users.noreply.github.com>
2023-09-16 19:17:12 +0900

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,4 @@
+x="Enter Celsius Temperature \n"
+Celsius=int(input(x))
+Fahrenheit = Celsius * 9 / 5 + 32 
+print("Fahrenheit Temperature :",Fahrenheit)
---
+++ 3.py
@@ -0,0 +1,11 @@
+x="Enter a number\n"
+second=int(input(x))
+a=second/3600
+print(type(a))
+b=a*60
+print(type(b))
+c=b*60
+value=round(c)
+print(type(b))
+print("the result using round :",value)
+print(second,"second is",a,"hours,",b,"minutes,",value,"seconds")
